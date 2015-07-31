3b46ab25756dcd790449c0c98b4b80360c1c876e
Grant George <grantelliottgeorge@gmail.com>
2015-07-31 15:19:36 -0700

    replace operation

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -12,30 +12,36 @@ angular.module('angularCalcApp')
 
     this.currentOutput = '';
     this.operations = '';
+    this.result = '';
 
     this.clickNumber = function(number) {
       this.currentOutput = this.currentOutput + number;
-    }
+    };
 
     this.operation = function(op) {
       this.operations +=  this.currentOutput;
       if (this.operations.indexOf('=') > 0) {
         this.operations = this.operations.slice(0, this.operations.indexOf('='));
       }
+      if (this.operations[this.operations.length-1].match(/[\/\+\-\*]/)) {
+        this.operations = this.operations.substring(0,this.operations.length-1);
+        this.operations += op;
+        return;
+      }
       this.operations += op;
       this.currentOutput = '';
-    }
+    };
 
     this.evaluate = function() {
       this.operations +=  this.currentOutput;
-      var result = eval(this.operations);
-      this.currentOutput = result;
-      this.operations += '=' + result;
-    }
+      this.result = eval(this.operations);
+      this.currentOutput = this.result.toString();
+      this.operations += '=' + this.result;
+    };
 
     this.clear = function() {
       this.currentOutput = '';
       this.operations = '';
-    }
+    };
 
   });
--- test/spec/controllers/calculator.js
+++ test/spec/controllers/calculator.js
@@ -17,14 +17,80 @@ describe('Controller: CalculatorCtrl', function () {
     });
   }));
 
-  describe('clickNumber function', function () {
-
-    it('clickNumber should concat the clicked number to currentOutput', function () {
+  describe('clickNumber()', function () {
+    it('should concat the clicked number to currentOutput when its blank', function () {
       CalculatorCtrl.currentOutput = '';
       CalculatorCtrl.clickNumber('3');
       expect(CalculatorCtrl.currentOutput).toBe('3');
     });
+    it('should concat the clicked number to currentOutput when its set', function () {
+      CalculatorCtrl.currentOutput = '3554';
+      CalculatorCtrl.clickNumber('3');
+      expect(CalculatorCtrl.currentOutput).toBe('35543');
+    });
+  });
 
+  describe('operation()', function() {
+    it('should add the current output to operations', function () {
+      CalculatorCtrl.currentOutput = '3';
+      CalculatorCtrl.operation('+');
+      expect(CalculatorCtrl.operations.indexOf('3') > -1).toBeTruthy();
+    });
+    it('should add operation to operations after the current output', function () {
+      CalculatorCtrl.currentOutput = '3';
+      CalculatorCtrl.operation('+');
+      expect(CalculatorCtrl.operations).toBe('3+');
+    });
+    it('should slice an = and operations thereafter', function () {
+      CalculatorCtrl.operations = '3+4=7';
+      CalculatorCtrl.operation('+');
+      expect(CalculatorCtrl.operations).toBe('3+4+');
+    });
+    it('should reset current output', function() {
+      CalculatorCtrl.currentOutput = '3';
+      CalculatorCtrl.operation('+');
+      expect(CalculatorCtrl.currentOutput).toBe('');
+    });
+    it('should replace an operator if it\'s the last operation', function() {
+      CalculatorCtrl.currentOutput = '3';
+      CalculatorCtrl.operation('+');
+      CalculatorCtrl.operation('-');
+      expect(CalculatorCtrl.operations).toBe('3-');
+    });
+  });
+
+  describe('evaluate()', function () {
+    it('should calculate a result', function() {
+      CalculatorCtrl.operations = '3+';
+      CalculatorCtrl.currentOutput = '4';
+      CalculatorCtrl.evaluate();
+      expect(CalculatorCtrl.result).toBe(7);
+    });
+    it('sets the current output to the result', function() {
+      CalculatorCtrl.operations = '3+';
+      CalculatorCtrl.currentOutput = '4';
+      CalculatorCtrl.evaluate();
+      expect(CalculatorCtrl.currentOutput).toBe('7');
+    });
+    it('should concat the current output and result to operations', function() {
+      CalculatorCtrl.operations = '3+';
+      CalculatorCtrl.currentOutput = '4';
+      CalculatorCtrl.evaluate();
+      expect(CalculatorCtrl.operations).toBe('3+4=7');
+    });
+  });
+
+  describe('clear()', function() {
+    it('should clear currentOutput', function() {
+      CalculatorCtrl.currentOutput = '9999';
+      CalculatorCtrl.clear();
+      expect(CalculatorCtrl.currentOutput).toBe('');
+    });
+    it('should clear operations', function() {
+      CalculatorCtrl.operations = '9*3=27';
+      CalculatorCtrl.clear();
+      expect(CalculatorCtrl.operations).toBe('');
+    });
   });
 
 });
